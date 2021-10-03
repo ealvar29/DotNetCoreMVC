7ee41576dd99766b4d7f0ab2fdb4ec276f4ca412
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-03 18:12:44 -0500

    tailwind added

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -1,3 +1,4 @@
+using BookStore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore
 {
@@ -23,6 +25,9 @@ namespace BookStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(
+                Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
