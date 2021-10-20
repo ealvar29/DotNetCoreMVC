4e6d7eeefdd2533b71b9273a89fac90fb84ad927
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-20 17:52:02 -0500

    new dotnet project and connection

---
+++ MVCDotNet5/MVCDotNet5/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCDotNet5.Models;
+
+namespace MVCDotNet5.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories {  get; set; }
+    }
+}
---
+++ MVCDotNet5/MVCDotNet5/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDotNet5.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
