21c14c545d13c28127a1b95e4cd43359c14901bd
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-21 08:32:50 -0500

    Category Edit

--- MVCDotNet5/MVCDotNet5/Controllers/CategoryController.cs
+++ MVCDotNet5/MVCDotNet5/Controllers/CategoryController.cs
@@ -36,5 +36,32 @@ namespace MVCDotNet5.Controllers
             }
             return View(category);
         }
+        //GET -- CREATE
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST -- Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
